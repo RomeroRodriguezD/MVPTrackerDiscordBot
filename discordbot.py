88cf427978c6a2899fb7082c39acb6ee9f9057b4
David <105886661+RomeroRodriguezD@users.noreply.github.com>
2022-11-24 14:52:47 +0100

    Add files via upload
    Base files.

---
+++ discordbot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import pandas as pd
+import datetime
+from datetime import timedelta
+import re
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix='!', description='MVP scheduler bot', intents=intents)
+
+@client.command(name='mvp')
+async def mvp(ctx, *, message):
+    context = message
+    context = str(context).lower()
+    list = pd.read_csv('roamingmvp.csv')
+    hour = datetime.datetime.now()
+    for mvp in list.itertuples():
+        print()
+        if context == str(mvp.MVP).lower():
+            if int(mvp.spawn) < 1000:
+                n = int(mvp.spawn)
+                respawn = hour + timedelta(minutes=n)
+                respawn_clean_hour = respawn.strftime('%H:%M:%S')
+                await ctx.send(f' MVP {context.title()} regresará a las {respawn_clean_hour}')
+            else:
+                await ctx.send(f'El MVP {context.title()} regresará tras {mvp.spawn} mob kills.')
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="si alguien mata algo"))
+
+client.run('MTAxMTkxNjgzODM0NDYwOTgxMg.GB4iFY.gSCA0lrCX_O_NmHEip0JethrBRAh5AokDuGlU0')
+
+
